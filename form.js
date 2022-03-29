85663c19a95f17cf5b4548c00594fbde0bd3f080
972007 <88716461+972007@users.noreply.github.com>
2022-03-29 15:06:14 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,8 @@
+class FORM {
+    constructor(){
+        this.input = createInput("").attribute("placeholder", "Enter your name");
+    this.playButton = createButton("Play");
+    this.titleImg = createImg("", "game title");
+    this.greeting = createElement("h2");
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,36 @@
+var space ,bg;
+var spacecraft,spacecraftImg ;
+
+function preload(){
+space = loadImage("space.png");   
+spacecraftImg = loadImage("spacecraft1.png");
+}
+
+function setup(){
+
+createCanvas(windowWidth,windowHeight);
+bg=createSprite(windowWidth/2,windowHeight/2,windowWidth,windowHeight);
+bg.addImage(space);
+bg.scale=1.75;
+spacecraft=createSprite(90,100,50,50);
+
+spacecraft.addImage(spacecraftImg);
+spacecraft.scale=0.2;
+}
+function draw(){
+    background("black");
+    
+    bg.velocityX=-3;
+
+    if(bg.x<700){
+        bg.x=bg.width/2;
+    }
+    if(keyDown(DOWN_ARROW)){
+        spacecraft.y=spacecraft.y+2;
+    }
+
+    if(keyDown(UP_ARROW)){
+        spacecraft.y=spacecraft.y-2;
+    }
+  drawSprites();
+}
